eed88c10c2b9004177fff5219ae8daacf8fac1d5
Mohamed Abdallah <moelsebeay10@gmail.com>
2025-05-09 20:02:37 +0300

    .

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,93 +20,101 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 200,
-                child: Image.asset(
-                  "assets/images/scanner.png",
-                  fit: BoxFit.contain,
-                ),
-              ),
-              const SizedBox(height: 32),
-
-              Form(
-                key: formKey,
+        child: Column(
+          children: [
+            SizedBox(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.20,
+                    child: Image.asset(
+                      "assets/images/logo2.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              child: Center(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    CustomTextForm(
-                      controller: email,
-                      label: const Text("Email"),
-                      obsc: false,
-                      txtInputType: TextInputType.emailAddress,
-                      validator: (value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return 'Email is required';
-                        } else if (!RegExp(
-                          r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-                        ).hasMatch(value)) {
-                          return 'Enter a valid email';
-                        }
-                        return null;
-                      },
-                    ),
-
-                    CustomTextForm(
-                      controller: password,
-                      label: const Text("Password"),
-                      suffixIcon: const Icon(Icons.visibility_off),
-                      obsc: true,
-                      txtInputType: TextInputType.text,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Password is required';
-                        } else if (value.length < 8) {
-                          return 'Password must be at least 8 characters long';
-                        } else if (!RegExp(r'[A-Z]').hasMatch(value)) {
-                          return 'Must contain at least one uppercase letter';
-                        } else if (!RegExp(r'[a-z]').hasMatch(value)) {
-                          return 'Must contain at least one lowercase letter';
-                        } else if (!RegExp(r'[0-9]').hasMatch(value)) {
-                          return 'Must contain at least one number';
-                        } else if (!RegExp(
-                          r'[!@#$%^&*(),.?":{}|<>]',
-                        ).hasMatch(value)) {
-                          return 'Must contain at least one special character';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 10),
-
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        "Forgot Password?",
-                        style: AppTextStyles.txt14f,
+                    
+                    const SizedBox(height: 32),
+                
+                    Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          CustomTextForm(
+                            controller: email,
+                            label: const Text("Email"),
+                            obsc: false,
+                            txtInputType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return 'Email is required';
+                              } else if (!RegExp(
+                                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                              ).hasMatch(value)) {
+                                return 'Enter a valid email';
+                              }
+                              return null;
+                            },
+                          ),
+                
+                          CustomTextForm(
+                            controller: password,
+                            label: const Text("Password"),
+                            suffixIcon: const Icon(Icons.visibility_off),
+                            obsc: true,
+                            txtInputType: TextInputType.text,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Password is required';
+                              } else if (value.length < 8) {
+                                return 'Password must be at least 8 characters long';
+                              } else if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                                return 'Must contain at least one uppercase letter';
+                              } else if (!RegExp(r'[a-z]').hasMatch(value)) {
+                                return 'Must contain at least one lowercase letter';
+                              } else if (!RegExp(r'[0-9]').hasMatch(value)) {
+                                return 'Must contain at least one number';
+                              } else if (!RegExp(
+                                r'[!@#$%^&*(),.?":{}|<>]',
+                              ).hasMatch(value)) {
+                                return 'Must contain at least one special character';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(height: 10),
+                
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              "Forgot Password?",
+                              style: AppTextStyles.txt14f,
+                            ),
+                          ),
+                          const SizedBox(height: 40),
+                
+                          CustomButton(
+                            txt: "Login",
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                Navigator.pushReplacementNamed(
+                                  context,
+                                  RouteName.scannerView,
+                                );
+                              }
+                            },
+                          ),
+                        ],
                       ),
-                    ),
-                    const SizedBox(height: 40),
-
-                    CustomButton(
-                      txt: "Login",
-                      onPressed: () {
-                        if (formKey.currentState!.validate()) {
-                          Navigator.pushReplacementNamed(
-                            context,
-                            RouteName.scannerView,
-                          );
-                        }
-                      },
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );

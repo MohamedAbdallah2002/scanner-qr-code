35b132f097596f1c776a7b50014428ce1a2d4461
Mohamed Abdallah <moelsebeay10@gmail.com>
2025-05-10 13:15:50 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:scann_qr_code/core/routes/route_handler.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:scann_qr_code/firebase_options.dart';
 
-void main() async {
+Future< void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Hive.initFlutter();
   await Hive.openBox<String>('scanned_codes');
   runApp(Task());
